ee68348b86f9d44268e736eb477d08f9c2184cfd
Martijn Versluis <martijnversluis@users.noreply.github.com>
2020-08-12 09:37:15 +0200

    Show hidden files by default

--- install.sh
+++ install.sh
@@ -320,3 +320,6 @@ mp__brew__ensure_cask_package_installed chromedriver \
 
 #                             Xcode     MainStage SSH Proxy GIPHY CAPTURE LastPass  Lockdown   Harvest   Boop
 mp__mas__ensure_app_installed 497799835 634159523 597790822 668208984     926036361 1483255076 506189836 1518425043
+
+defaults write com.apple.finder AppleShowAllFiles YES
+killall Finder /System/Library/CoreServices/Finder.app
